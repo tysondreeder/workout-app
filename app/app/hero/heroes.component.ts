78459572745722fc19d1a444bd7a28ae6cd4f455
Mike <mike@mbautista.com>
2016-08-21 16:05:25 -0500

    Committing files for the hero component

--- app/app/hero/heroes.component.ts
+++ app/app/hero/heroes.component.ts
@@ -1,72 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { Hero } from './hero/hero';
-import { HeroService } from './hero/hero.services'
+import { Hero } from './hero';
+import { HeroService } from './hero.services'
 
 @Component({
     selector: 'my-heroes',
-    template:`
-        <h1>{{title}}</h1>
-        <h2>My Heroes</h2>
-        <ul class="heroes">
-            <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
-                <span class="badge">{{hero.id}}</span> {{hero.name}}
-            </li>
-        </ul>
-        <my-hero-detail [hero]="selectedHero"></my-hero-detail>`,
-    styles: [`
-          .selected {
-            background-color: #CFD8DC !important;
-            color: white;
-          }
-          .heroes {
-            margin: 0 0 2em 0;
-            list-style-type: none;
-            padding: 0;
-            width: 15em;
-          }
-          .heroes li {
-            cursor: pointer;
-            position: relative;
-            left: 0;
-            background-color: #EEE;
-            margin: .5em;
-            padding: .3em 0;
-            height: 1.6em;
-            border-radius: 4px;
-          }
-          .heroes li.selected:hover {
-            background-color: #BBD8DC !important;
-            color: white;
-          }
-          .heroes li:hover {
-            color: #607D8B;
-            background-color: #DDD;
-            left: .1em;
-          }
-          .heroes .text {
-            position: relative;
-            top: -3px;
-          }
-          .heroes .badge {
-            display: inline-block;
-            font-size: small;
-            color: white;
-            padding: 0.8em 0.7em 0 0.7em;
-            background-color: #607D8B;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -4px;
-            height: 1.8em;
-            margin-right: .8em;
-            border-radius: 4px 0 0 4px;
-          }
-        `]
+    templateUrl: './app/hero/heroes.component.html',
+    styleUrls: ['./app/hero/heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+    heroes: Hero[];
+    selectedHero: Hero;
 
-    constructor(private heroService: HeroService) {}
+    constructor(
+        private router: Router,
+        private heroService: HeroService
+    ) {}
 
     getHeroes() {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -76,11 +26,12 @@ export class HeroesComponent implements OnInit {
         this.getHeroes();
     }
 
-    heroes: Hero[];
-    title ="Tour of Heros";
-    selectedHero: Hero;
     onSelect(hero: Hero) {
         this.selectedHero = hero;
+    }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id])
     }
 }
 /**
